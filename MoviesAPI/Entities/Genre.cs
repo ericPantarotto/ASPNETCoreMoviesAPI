83a9376ca26db253201fab7ad5230bd1b600251e
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-01-20 18:41:14 +0100

    Custom Validation

--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -1,12 +1,38 @@
+using MoviesAPI.Validations;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesAPI.Entities
 {
-    public class Genre
+    public class Genre: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "The field with FieldName '{0}' is required")]
         [StringLength(10)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
+
+        //IValidatableObject is hard-coded at the model level while ValidationAttribute can be re-used
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                string firstLetter = Name[0].ToString();
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be upper case", new string[] { nameof(Name) });
+                }
+
+                //if (Name.Length > 5)
+                //{
+                //    yield return new ValidationResult("why is it greater than 5", new string[] { nameof(Name) });
+                //}
+            }
+            //if (Id > 0)
+            //{
+            //    yield return new ValidationResult("Id shouldn't be provided!", new string[] { nameof(Name) });
+            //}
+
+        }
     }    
 }
---
+++ MoviesAPI/Validations/FirstLetterUppercaseAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Validations
+{
+    public class FirstLetterUppercaseAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            string firstLetter = value.ToString()[0].ToString();
+            if (firstLetter  != firstLetter.ToUpper())
+            {
+                return new ValidationResult("First letter should be upper case");
+            }
+            return ValidationResult.Success;
+
+        }
+    }
+}
