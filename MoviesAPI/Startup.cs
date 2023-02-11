15ddf74579cb7ce8cc5c19e4d1c3e8d132f67217
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-11 23:08:35 +0100

    Azure Deploy

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -4,6 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MoviesAPI.Configuration;
+using System;
+using Microsoft.Extensions.Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
 
 namespace MoviesAPI
 {
