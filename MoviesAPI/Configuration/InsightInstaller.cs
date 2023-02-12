9931788eebff61a5aab2d3347546ff3a06e17283
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2023-02-12 23:50:21 +0100

    application Insight

---
+++ MoviesAPI/Configuration/InsightInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MoviesAPI.Configuration
+{
+    public class InsightInstaller : IServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplicationInsightsTelemetry();
+        }
+    }
+}
--- MoviesAPI/Controllers/RootController.cs
+++ MoviesAPI/Controllers/RootController.cs
@@ -13,13 +13,14 @@ namespace MoviesAPI.Controllers
         [HttpGet(Name = "getRoot")]
         public ActionResult<IEnumerable<Link>> Get()
         {
-            List<Link> links = new List<Link>();
-
-            links.Add(new Link(href: Url.Link("getRoot", new { }), rel: "self", method: "GET"));
-            links.Add(new Link(href: Url.Link("createUser", new { }), rel: "create-user", method: "POST"));
-            links.Add(new Link(href: Url.Link("Login", new { }), rel: "login", method: "POST"));
-            links.Add(new Link(href: Url.Link("getGenres", new { }), rel: "get-genres", method: "GET"));
-            links.Add(new Link(href: Url.Link("getPeople", new { }), rel: "get-people", method: "GET"));
+            List<Link> links = new()
+            {
+                new Link(href: Url.Link("getRoot", new { }), rel: "self", method: "GET"),
+                new Link(href: Url.Link("createUser", new { }), rel: "create-user", method: "POST"),
+                new Link(href: Url.Link("Login", new { }), rel: "login", method: "POST"),
+                new Link(href: Url.Link("getGenres", new { }), rel: "get-genres", method: "GET"),
+                new Link(href: Url.Link("getPeople", new { }), rel: "get-people", method: "GET")
+            };
 
             return links;
         }
